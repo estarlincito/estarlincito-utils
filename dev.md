c7d9e10c3e9bab4367509f6929f9a6ecba0d644d
Estarlin R <dev@estarlincito.com>
2025-02-13 11:10:11 -0400

    @estarlincito/utils@1.1.2

---
+++ dev.md
@@ -0,0 +1,17 @@
+```bash
+npm login
+```
+
+```bash
+npm publish --access public
+```
+
+```bash
+git add .
+git commit -m "$"
+git push
+```
+
+```bash
+rm -rf dist && pnpm tsc && npm publish --access public
+```
--- dist/metadata.js
+++ dist/metadata.js
@@ -13,14 +13,27 @@
  *    - `public/assets/favicons/shortcut-icon.png`
  */
 export const generateMetadata = (meta) => {
-    return {
-        title: meta.title,
-        description: meta.description,
-        metadataBase: new URL(meta.url.hostname),
-        openGraph: meta,
-        icons: {
-            icon: '/assets/favicons/favicon.ico',
-            shortcut: '/assets/favicons/shortcut-icon.png',
-        },
-    };
+  return {
+    title: meta.title,
+    description: meta.description,
+    metadataBase: new URL(meta.url.origin),
+    openGraph: meta,
+    icons: {
+      icon: '/assets/favicons/favicon.ico',
+      shortcut: '/assets/favicons/shortcut-icon.png',
+    },
+  };
 };
+generateMetadata({
+  type: 'book',
+  locale: 'en-US',
+  siteName: 'MyEbook',
+  authors: ['doc.authors'],
+  isbn: 'doc.isbn',
+  releaseDate: 'doc.released',
+  tags: ['doc.tags'],
+  description: 'doc.summary',
+  title: 'doc.title',
+  images: [{ width: 800, height: 600, url: 'doc.cover', alt: 'doc.coverAl' }],
+  url: new URL(`https://myebook.estarlincito.com/ebooks/`),
+});
